5048f0892d8fde521b34ffc0f0b0874b72eec330
Karolina Juszczak <karolinajuszczak98@gmail.com>
2021-05-07 17:26:16 +0200

    Add styling and button functionality

--- public/js/text-editor.js
+++ public/js/text-editor.js
@@ -1,7 +1,21 @@
 // enabling text area on
 
-function enableText() {
-  textEditorField.document.designMode = "On";
+textEditorField.document.designMode = "On";
+
+//edit buttons
+
+const editButtons = document.querySelectorAll(".text-editor__edit-btns");
+
+for (let i = 0; i < editButtons.length; i++) {
+  editButtons[i].addEventListener("click", () => {
+    let cmd = editButtons[i].getAttribute("data-cmd");
+
+    if (cmd === "bold" || cmd === "italic" || cmd === "insertUnorderedList") {
+      textEditorField.document.execCommand(cmd, false, null);
+    }
+  });
 }
 
-enableText();
+// save - read buttons
+
+
